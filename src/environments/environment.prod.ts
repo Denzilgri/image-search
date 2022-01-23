bacefb48c4e271208547458044da3f73b23d4bec
Denzil Godwin Griffiths <2544868G@student.gla.ac.uk>
2022-01-23 23:59:40 +0000

    chore: Update to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   flickr: {
-    apiKey: '9135c8fba194f49f6f5676927791040f'
+    apiKey: '9135c8fba194f49f6f5676927791040f',
+    url: 'https://api.flickr.com/services/rest/',
+    imageUrl: 'https://live.staticflickr.com/'
   }
 };
